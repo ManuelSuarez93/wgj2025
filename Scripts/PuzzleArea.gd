2180c1234fa1d4f2a61474dba669683add14cfe2
Manuel Suarez <82424347+ManuelSuarez93@users.noreply.github.com>
2025-08-17 16:51:20 -0300

    fixes

--- Scripts/PuzzleArea.gd
+++ Scripts/PuzzleArea.gd
@@ -3,6 +3,7 @@ extends Node3D
 class_name PuzzleArea
 
 @export var Door : Door 
+@export var PuzzleType : String
 
 var puzzles : Array[Triggerable]
 var finishedPuzzles: Array[Triggerable]
@@ -10,7 +11,7 @@ var finishedPuzzles: Array[Triggerable]
 signal puzzleFinished
 
 func checkForPuzzles():
-	for children in self.get_children():
+	for children in get_tree().get_nodes_in_group(PuzzleType):
 		if children is Triggerable:
 			puzzles.append(children)
 			children.onTrigger.connect(puzzleSolved)
--- Scripts/Triggerable.gd
+++ Scripts/Triggerable.gd
@@ -2,7 +2,6 @@ extends Area3D
 
 class_name Triggerable
 
-@export var MainPuzzleArea : PuzzleArea
 @export var canTrigger : bool  = true
 
 signal onTrigger(Puzzle : Triggerable)
